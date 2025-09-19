ea529f3aaa1ec37e21471b189acae1d77a0aa35f
grufre349 <grufre349@gmail.com>
2025-09-19 11:40:55 +0200

    added missing checks for placeholder texts

--- src/components/ContactFrom.test.tsx
+++ src/components/ContactFrom.test.tsx
@@ -22,7 +22,9 @@ describe("Test the Contact Form Component", () => {
 		it("Test input fields after initial rendering", () => {
 			// arrange
 			render(<ContactForm />);
+
 			// act -> nothing to do because its still initial and static
+			// -------------------------------------------------------------
 			// assert for Label for the name input
 			// Best Practice because of Screenreaders and to test Linking between Labels and Inputs
 			expect(
@@ -32,26 +34,47 @@ describe("Test the Contact Form Component", () => {
 			expect(
 				screen.getByRole("textbox", { name: /message/i })
 			).toBeInTheDocument();
+			// check for message field
+			expect(
+				screen.getByRole("textbox", { name: /message/i })
+			).toBeInTheDocument();
+
+			// check for placeholder texts seperately wiith another method
+			expect(
+				screen.getByPlaceholderText(/enter your name/i)
+			).toBeInTheDocument();
+			expect(
+				screen.getByPlaceholderText(/enter your email/i)
+			).toBeInTheDocument();
+			expect(
+				screen.getByPlaceholderText(/enter your message/i)
+			).toBeInTheDocument();
+
 			// assert checkbox for newsletter
 			expect(
 				screen.getByRole("checkbox", { name: /subscribe/i })
 			).toBeInTheDocument();
+			// check if Button is enabled
 			expect(
 				screen.getByRole("button", { name: /send message/i })
 			).toBeEnabled();
+
 			// expect NO messages with following text snippets
 			expect(screen.queryByText(/is required/i)).toBeNull();
 			expect(screen.queryByText(/is invalid/i)).toBeNull();
 		});
 	});
+
+	// second test suite
 	describe("Client Side Error Validation", () => {
 		it("Show 'name is required' when name is empty", async () => {
 			// arrange
 			render(<ContactForm />);
 			const form = screen.getByRole("form");
 			// act
+			// use submit methob on form elements
 			fireEvent.submit(form);
-			// assert
+			// assert with an async waitFor because of delay
 			await waitFor(() => {
 				expect(
 					screen.queryByText(/name is required/i)
